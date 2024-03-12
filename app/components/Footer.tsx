701f8e2ba9d3e894ced688f11ad931bacb506396
taevel02 <taevel02@gmail.com>
2024-03-12 21:26:30 +0900

    chore(style): change font weight

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="mt-16 text-center">
-      <p className="text-sm">&copy; 2024 Taehoon Kwon</p>
+      <p className="text-sm font-thin">&copy; 2024 Taehoon Kwon</p>
     </footer>
   );
 }
