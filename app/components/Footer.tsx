59aee7ef4b233783b7b489eeaf746d87b5922e17
taevel02 <taevel02@gmail.com>
2024-03-17 18:22:28 +0900

    chore(ui): modify margin width

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="mt-16 text-center">
+    <footer className="my-16 text-center">
       <p className="text-sm font-thin">&copy; 2024 Taehoon Kwon</p>
     </footer>
   );
