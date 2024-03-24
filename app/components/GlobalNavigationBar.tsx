5be250d0ab5202db207bc436e6f37dab5ffb947a
taevel02 <taevel02@gmail.com>
2024-03-24 12:33:22 +0900

    fix: add aria-label to button

--- app/components/GlobalNavigationBar.tsx
+++ app/components/GlobalNavigationBar.tsx
@@ -35,7 +35,12 @@ function ModeToggle() {
   };
 
   return (
-    <Button variant="ghost" size="icon" onClick={() => toggle()}>
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => toggle()}
+      aria-label="ModeToggle"
+    >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     </Button>
