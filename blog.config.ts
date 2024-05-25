2f1aa4e786b8e97767f6d2cc22f9dac8700e8cd2
Taehoon Kwon <taevel02@gmail.com>
2024-05-25 12:43:03 +0900

    Update blog.config.ts

--- blog.config.ts
+++ blog.config.ts
@@ -18,9 +18,10 @@ export default {
     id: "G-GHV52J2X34", // G-XXXXXXXXXX
   },
   about: `
-    소프트웨어 엔지니어입니다.<br />
-    도시에서는 전시를, 자연에서는 스쿠버다이빙과 스키를 즐깁니다.<br /><br />
+    삶을 헤엄치고 있습니다.<br />
+    글과 사랑, 그리고 행복을 추구합니다.<br /><br />
 
+    책을 좋아합니다.<br />
     저의 <a href="https://abit.ly/taehoon-bookshelf">서재</a>를 둘러보세요.
   `,
 };
