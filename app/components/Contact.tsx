7a3047bc2ec57cd1a2e64f050493b246c5cef3dc
taevel02 <taevel02@gmail.com>
2024-11-07 20:03:03 +0800

    feat: change icon system

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,14 +1,14 @@
-import { Instagram, Mail } from "lucide-react";
+import { RiMailLine, RiInstagramLine } from "@remixicon/react";
 
 export default function ContactList() {
   return (
     <div className="flex gap-6 max-[320px]:flex-col max-[320px]:gap-0">
       <div className="flex items-center gap-2">
-        <Mail size={16} />
+        <RiMailLine size={16} />
         <a href="mailto:taevel02@gmail.com">taevel02@gmail.com</a>
       </div>
       <div className="flex items-center gap-2">
-        <Instagram size={16} />
+        <RiInstagramLine size={16} />
         <a href="https://www.instagram.com/taevel02/">taevel02</a>
       </div>
     </div>
--- app/components/GlobalNavigationBar.tsx
+++ app/components/GlobalNavigationBar.tsx
@@ -1,6 +1,6 @@
 import { NavLink } from "@remix-run/react";
 import { Theme, useTheme } from "remix-themes";
-import { Moon, Sun } from "lucide-react";
+import { RiMoonLine, RiSunLine } from "@remixicon/react";
 
 import { Button } from "./ui/Button";
 
@@ -40,8 +40,8 @@ function ModeToggle() {
       onClick={() => toggle()}
       aria-label="ModeToggle"
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <RiSunLine className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <RiMoonLine className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     </Button>
   );
 }
