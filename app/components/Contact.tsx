f056d6d219c02c453118c936050d206a483ab69c
taevel02 <taevel02@gmail.com>
2024-12-18 11:58:30 +0800

    style: modify contact sorting

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -2,7 +2,7 @@ import { RiMailLine, RiInstagramLine } from "@remixicon/react";
 
 export default function ContactList() {
   return (
-    <div className="flex gap-6 max-[320px]:flex-col max-[320px]:gap-0">
+    <div className="flex flex-col">
       <div className="flex items-center gap-2">
         <RiMailLine size={16} />
         <a href="mailto:taevel02@gmail.com">taevel02@gmail.com</a>
