dadd779c4b1b104fde0e79bcbcbabb4672237c72
taevel02 <taevel02@gmail.com>
2025-06-16 16:21:56 +0800

    feat: change to current year

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,9 @@
+import dayjs from "dayjs";
+
 export function Footer() {
   return (
     <footer className="my-16 text-center">
-      <p className="text-sm font-thin">&copy; 2024 Taehoon Kwon</p>
+      <p className="text-sm font-thin">&copy; {dayjs().year()} Taehoon Kwon</p>
     </footer>
   );
 }
