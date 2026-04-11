8ad520c8189e00c8159fb23cab6cbaa3118aaf31
taevel02 <taevel02@gmail.com>
2026-04-11 18:20:27 +1000

    chore: update favicon and align manifest with Single Fetch

--- app/routes/manifest[.]webmanifest.ts
+++ app/routes/manifest[.]webmanifest.ts
@@ -1,28 +1,26 @@
-import { json } from "@remix-run/node";
 import type { LoaderFunction } from "@remix-run/node";
 
 export const loader: LoaderFunction = async () => {
-  return json(
-    {
-      name: "Taehoon Kwon",
-      short_name: "Taehoon",
-      start_url: "/",
-      display: "standalone",
-      background_color: "#ffffff",
-      theme_color: "#ffffff",
-      icons: [
-        {
-          src: "/favicon.ico",
-          sizes: "64x64 32x32 24x24 16x16",
-          type: "image/x-icon",
-        },
-      ],
-    },
-    {
-      headers: {
-        "Cache-Control": "public, max-age=600",
-        "Content-Type": "application/manifest+json",
+  const manifest = {
+    name: "Taehoon Kwon",
+    short_name: "Taehoon",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "64x64 32x32 24x24 16x16",
+        type: "image/x-icon",
       },
-    }
-  );
+    ],
+  };
+
+  return new Response(JSON.stringify(manifest), {
+    headers: {
+      "Cache-Control": "public, max-age=600",
+      "Content-Type": "application/manifest+json",
+    },
+  });
 };
