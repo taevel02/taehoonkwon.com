455fa79bb9514f6203f141678bdf59642317d9f5
taevel02 <taevel02@gmail.com>
2026-07-03 12:46:53 +1000

    feat: replace location with Twitter link in Contact component

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,11 +1,11 @@
-import { RiMailLine, RiMapPin2Line } from "@remixicon/react";
+import { RiMailLine, RiTwitterXFill } from "@remixicon/react";
 
 export default function ContactList() {
   return (
     <address className="flex flex-col gap-1 not-italic text-sm">
       <div className="flex items-center gap-2">
-        <RiMapPin2Line size={16} className="text-muted-foreground" />
-        <span>Australia</span>
+        <RiTwitterXFill size={16} className="text-muted-foreground" />
+        <a href="https://x.com/theokwon__">@theokwon__</a>
       </div>
       <div className="flex items-center gap-2">
         <RiMailLine size={16} className="text-muted-foreground" />
