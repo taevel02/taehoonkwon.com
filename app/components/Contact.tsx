6c7507f2d62efbf3b748fb1229a4bd180bf4a679
taevel02 <taevel02@gmail.com>
2026-07-06 19:39:49 +1000

    refactor: remove Twitter link from contact information section

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -4,10 +4,6 @@ export default function ContactList() {
   return (
     <address className="flex flex-col gap-1 not-italic text-sm">
       <div className="flex items-center gap-2">
-        <RiTwitterXFill size={16} className="text-muted-foreground" />
-        <a href="https://x.com/theokwon__">@theokwon__</a>
-      </div>
-      <div className="flex items-center gap-2">
         <RiMailLine size={16} className="text-muted-foreground" />
         <a href="mailto:taevel02@gmail.com">taevel02@gmail.com</a>
       </div>
