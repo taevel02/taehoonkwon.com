3ee9ad30bdc367cf86294b13ea70ae6429396358
taevel02 <taevel02@gmail.com>
2026-08-12 21:58:42 +1000

    feat: add Threads contact

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,4 +1,4 @@
-import { RiMailLine, RiTwitterXFill } from "@remixicon/react";
+import { RiMailLine, RiThreadsLine } from "@remixicon/react";
 
 export default function ContactList() {
   return (
@@ -6,6 +6,10 @@ export default function ContactList() {
       <div className="flex items-center gap-2">
         <RiMailLine size={16} className="text-muted-foreground" />
         <a href="mailto:taevel02@gmail.com">taevel02@gmail.com</a>
+      </div>
+      <div className="flex items-center gap-2">
+        <RiThreadsLine size={16} className="text-muted-foreground" />
+        <a href="https://www.threads.com/@theokwon_">theokwon_</a>
       </div>
     </address>
   );
